453469c7413abbacf468898ed99026fadf775c04
Vladislav Knyshov <inova136@gmail.com>
2023-11-19 19:12:19 +0100

    feat: add center prop to heading

--- materials-client/src/ui/Heading/Heading.tsx
+++ materials-client/src/ui/Heading/Heading.tsx
@@ -5,14 +5,20 @@ import styles from "./Heading.module.css";
 export type HeadingProps = {
   children?: string | number | null;
   level?: "h1" | "h2" | "h3";
+  center?: boolean;
 } & HTMLProps<HTMLHeadingElement>;
 
 const Heading: FC<HeadingProps> = ({
   level = "h1",
   children,
+  center,
   ...headingProps
 }) => {
-  const className = cn(styles.heading, styles[`${level}Heading`]);
+  const className = cn(
+    styles.heading,
+    styles[`${level}Heading`],
+    center && styles.centerHeading
+  );
   return createElement(level, { className, ...headingProps }, children);
 };
 
