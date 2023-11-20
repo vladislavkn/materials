0c833a525eff7709a06c6749872338a75835be27
Vladislav Knyshov <inova136@gmail.com>
2023-11-20 12:53:43 +0100

    feat: add cors

--- materials-server/index.js
+++ materials-server/index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const loggerMiddleware = require("./middleware/logger");
+const cors = require("cors");
 
 const authRouter = require("./auth");
 const materialsRouter = require("./materials");
 
 const app = express();
 const port = process.env.PORT;
+const origin = process.env.ORIGIN;
 
+app.use(cors({ origin }));
 app.use(loggerMiddleware);
 app.use(express.json());
 app.use("/auth", authRouter);
